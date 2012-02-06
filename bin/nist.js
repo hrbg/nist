f349327d70237cd42bfd63e1a0f3f6f40d458a04
Hector Bavio <hectorbavio@gmail.com>
2012-02-06 19:12:30 -0300

    Success to post to congo server

---
+++ bin/nist.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var util = require("util")
+var nist = require("../lib/nist")
+
+var args = process.argv.slice(0);
+// shift off node and script name
+args.shift(); args.shift();
+console.log("------")
+console.log(util.inspect(nist, true, null));
+console.log("------")
+nist.file(args[0])
