e2e10571f3a72c3bc66905177be0036ba266d277
Hector Bavio <hectorbavio@gmail.com>
2012-02-08 19:46:05 -0300

    Parse Json respond

--- bin/nist.js
+++ bin/nist.js
@@ -6,7 +6,7 @@ var nist = require("../lib/nist")
 var args = process.argv.slice(0);
 // shift off node and script name
 args.shift(); args.shift();
-console.log("------")
-console.log(util.inspect(nist, true, null));
-console.log("------")
+// console.log("------")
+// console.log(util.inspect(nist, true, null));
+// console.log("------")
 nist.file(args[0])
