50066e6d306da7b431f9b1f3ea7f643ca87202f4
Hector Bavio <hectorbavio@gmail.com>
2012-02-10 15:39:58 -0300

    Add config ~/nistrc file

--- bin/nist.js
+++ bin/nist.js
@@ -1,12 +1,37 @@
 #!/usr/bin/env node
 
-var util = require("util")
-var nist = require("../lib/nist")
+var util    = require("util");
+var path    = require("path");
+var nist    = require("../lib/nist");
+var program = require('commander');
+// var version = require("../lib/version")
 
-var args = process.argv.slice(0);
-// shift off node and script name
-args.shift(); args.shift();
+// var args = process.argv.slice(0);
+// // shift off node and script name
+// args.shift(); args.shift();
 // console.log("------")
-// console.log(util.inspect(nist, true, null));
+// // console.log(util.inspect(nist, true, null));
 // console.log("------")
-nist.file(args[0])
+
+// nist.readFile(args[0])
+// nist.openNistrcFile()
+
+program
+  .version('0.0.1')
+  .option('-c, --config', 'Github account file')
+  .parse(process.argv);
+
+//Search for ~/.nistrc and prompt for Github account if is not found
+path.exists(nist.nistrcPath, function (exists) {
+  if(!exists || program.config) {
+    program.prompt('Login or Email: ', function(name){
+      program.password('Password: ', '*', function(pass){
+        process.stdin.destroy();
+        nist.openNistrcFile(JSON.stringify({"login": name, "pass": pass}))
+        if(program.args.length > 0) nist.readFile(program.args[0])
+      });
+    });
+  }
+});
+
+console.log(program.args)
