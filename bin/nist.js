9a05d99b18562ee100de53e7b9851faf2e7e23dc
Hector Bavio <hectorbavio@gmail.com>
2012-02-11 15:04:58 -0300

    Clear commented code

--- bin/nist.js
+++ bin/nist.js
@@ -6,16 +6,6 @@ var nist    = require("../lib/nist");
 var program = require('commander');
 // var version = require("../lib/version")
 
-// var args = process.argv.slice(0);
-// // shift off node and script name
-// args.shift(); args.shift();
-// console.log("------")
-// // console.log(util.inspect(nist, true, null));
-// console.log("------")
-
-// nist.readFile(args[0])
-// nist.openNistrcFile()
-
 program
   .version('0.0.1')
   .option('-c, --config', 'Github account file')
@@ -35,18 +25,3 @@ nist.findNistrcFile(function (exists) {
     if(program.args.length > 0) nist.readFile(program.args[0])    
   }
 });
-
-//Search for ~/.nistrc and prompt for Github account if is not found
-// path.exists(nist.nistrcPath, function (exists) {
-//   if(!exists || program.config) {
-//     program.prompt('Login or Email: ', function(name){
-//       program.password('Password: ', '*', function(pass){
-//         process.stdin.destroy();
-//         nist.openNistrcFile(JSON.stringify({"login": name, "pass": pass}))
-//         if(program.args.length > 0) nist.readFile(program.args[0])
-//       });
-//     });
-//   }
-// });
-
-console.log(program.args)
